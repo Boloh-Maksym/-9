c31cc02ba565097d838f6e69b8b49cb621bfd52c
Boloh-Maksym <bolohmaksym2005@gmail.com>
2024-07-07 16:50:16 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int min_steps(int x, int y) {
+    int distance = y - x;
+    if (distance == 0) {
+        return 0;
+    }
+
+    int steps = 0;
+    int current_position = x;
+    int step_size = 1;
+
+    while (current_position < y) {
+        if (current_position + step_size <= y) {
+            current_position += step_size;
+        } else {
+            step_size = 1;
+            current_position += step_size;
+        }
+        steps++;
+        if (current_position == y) {
+            break;
+        }
+        if (current_position + (step_size + 1) <= y) {
+            step_size++;
+        } else if (current_position + step_size <= y) {
+
+        } else {
+            step_size--;
+        }
+    }
+
+    return steps;
+}
+
+int main() {
+    int x, y;
+    printf("¬вед≥ть x: ");
+    scanf("%d", &x);
+    printf("¬вед≥ть y: ");
+    scanf("%d", &y);
+
+    int result = min_steps(x, y);
+    printf("ћ≥н≥мальна к≥льк≥сть крок≥в: %d\n", result);
+
+    return 0;
+}
+
